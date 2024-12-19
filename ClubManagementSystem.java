bf2cfffd8038aa9ee4ccea27a18b8e71407ed523
kjw <jiwoonkim01@naver.com>
2024-12-19 22:12:13 +0900

    프로젝트 설명

--- ClubManagementSystem.java
+++ ClubManagementSystem.java
@@ -1,3 +1,13 @@
+/**
+ * 청주대학교 동아리 관리 시스템
+ * <p>
+ * 관리자, 학생, 동아리 회장을 위한 GUI 기반의 동아리 관리 시스템입니다.
+ * </p>
+ *
+ * @author Ji Woon Kim
+ * @version 1.0
+ * @since 2024-12-18
+ */
 public class ClubManagementSystem {
 
 }
